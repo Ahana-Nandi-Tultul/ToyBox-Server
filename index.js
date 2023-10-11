97b752ecba3f218e630da35ce5e6fb6478ca7df2
Ahana Nandi <ahananondi6@gmail.com>
2023-10-11 21:40:04 +0600

    try to solve cors problem

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://toybox-97639.web.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
